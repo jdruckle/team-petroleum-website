d2e138a05cb8048ef0d7453fb5a771ca7e497e69
Jordan Ruckle <jordanruckle@yahoo.com>
2019-04-02 13:01:13 -0600

    Trying to add smoothscroll

--- js/script.js
+++ js/script.js
@@ -253,3 +253,17 @@
     AOS.init();
 
 })(jQuery);
+
+// $(document).ready(function() {
+//     /*  smoothscroll
+//     ----------------------------------------------*/
+//      $(function() {
+//           $('#home a, .navbar-default a').bind('click', function(event) {
+//               var $anchor = $(this);
+//               $('html, body').stop().animate({
+//                   scrollTop: $($anchor.attr('href')).offset().top - 49
+//               }, 1000);
+//               event.preventDefault();
+//           });
+//       });
+//     });
