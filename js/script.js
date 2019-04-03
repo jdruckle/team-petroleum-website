5d9868c11bea689d0450cd10fa3f0bbd409436f0
Jordan Ruckle <jordanruckle@yahoo.com>
2019-04-02 19:14:49 -0600

    Added + signs to counter JS

--- js/script.js
+++ js/script.js
@@ -213,7 +213,25 @@
             oTop = $('.count').offset().top - window.innerHeight;
         }
         if ($(window).scrollTop() > oTop) {
-            $('.count').each(function () {
+            $('.count.text-plus').each(function () {
+                var $this = $(this),
+                    countTo = $this.attr('data-count');
+                $({
+                    countNum: $this.text()
+                }).animate({
+                    countNum: countTo
+                }, {
+                    duration: 1000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum + "+");
+                    }
+                });
+            });
+            $('.count.no-text-plus').each(function () {
                 var $this = $(this),
                     countTo = $this.attr('data-count');
                 $({
